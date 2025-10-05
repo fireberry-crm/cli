f12ebb78ea532438785ce04aced0c8df7a9b587b
Nimrod Silberfeld <nimrods@fireberry.com>
2025-10-05 11:44:45 +0300

    feat: fb-5386 create local app

--- src/bin/fireberry.ts
+++ src/bin/fireberry.ts
@@ -2,6 +2,7 @@
 import { Command } from "commander";
 import chalk from "chalk";
 import { runInit } from "../commands/init.js";
+import { runCreate } from "../commands/create.js";
 import packageJson from "../../package.json" with { type: "json" };
 
 const program = new Command();
@@ -17,6 +18,14 @@ program
   .description("Initiates credentials and stores token in local config")
   .action(async (tokenid?: string) => {
     await runInit({ tokenid });
+  });
+
+program
+  .command("create")
+  .argument("[name]", "App name")
+  .description("Create a new Fireberry app")
+  .action(async (name?: string) => {
+    await runCreate({ name });
   });
 
 program.parseAsync(process.argv).catch((err: unknown) => {
