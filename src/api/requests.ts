ffce785c0535aa78cf794026f0a5b8deaa6a6bf3
Nimrod Silberfeld <nimrods@fireberry.com>
2025-11-27 10:58:43 +0200

    fix delete route

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,10 +1,6 @@
 import "../config/env.js";
 import { api } from "./axios.js";
-import type {
-  CreateAppRequest,
-  Manifest,
-  ZippedComponent,
-} from "./types.js";
+import type { CreateAppRequest, Manifest, ZippedComponent } from "./types.js";
 
 export const createApp = async (data: CreateAppRequest): Promise<void> => {
   const url = "/services/developer/create";
@@ -37,9 +33,9 @@ export const installApp = async (manifest: Manifest): Promise<void> => {
 };
 
 export const deleteApp = async (manifest: Manifest): Promise<void> => {
-  const url = `/services/developer/delete/${manifest.app.id}`;
+  const url = `/services/developer/delete`;
   try {
-    await api.delete<void>(url);
+    await api.delete<void>(url, manifest);
   } catch (error) {
     throw new Error(error instanceof Error ? error.message : "Unknown error");
   }
